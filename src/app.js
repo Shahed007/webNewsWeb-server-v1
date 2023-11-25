a84ceb3dad6b21c011effbedd325d5b9b4d38d94
Shahed <sk0178397@gmial.com>
2023-11-25 20:57:54 +0600

    Connect mongoose with mongoodb database and created error handaling route.

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const connectDB = require("./db/connectDB");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get("/health", (req, res) => {
+  res.send("my webNewsWaveDB server is ruining");
+});
+
+app.all("*", (req, res, next) => {
+  const error = new Error(`the requested url is invalid :${req.url}`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+  });
+});
+
+const main = async () => {
+  await connectDB();
+  app.listen(port, () => {
+    console.log(`my server is ruining prot ${port}`);
+  });
+};
+
+main();
